9447e2511bcaf42d040689e168aa330a2ad32f68
Fausnightm <m.fausnight@lafilm.edu>
2021-10-24 19:19:54 -0400

    run build

---
+++ client/build/precache-manifest.51bf203e834a3a8d56b8dc8577e45a42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ce0782b5eec1b0f05e7c8a588eb4ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2081d60272cf6bb2a437",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "1aeabbe6a3921d5e0d34",
+    "url": "/static/js/2.793cd252.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.793cd252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2081d60272cf6bb2a437",
+    "url": "/static/js/main.d9fe425f.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
